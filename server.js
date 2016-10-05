dfad8d441a194922ab12d4ff21dbc3b0f0556e14
Raphael Constantino <raphaelc23@gmail.com>
2016-10-05 11:04:06 +0000

    change port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var useragent = require('express-useragent');
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 var app = express();
 
 app.use(bodyParser.json());
